d13761c8703cd96796d495fe1160a1adebf1c9d8
Killa J <joshuaalexandercross@gmail.com>
2021-07-23 19:06:39 -0500

    updated dashboard route

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -39,6 +39,43 @@ router.get('/', (req, res) => {
     });
 });
 
+router.get('/dashboard', (req, res) => {
+  console.log(req.session);
+
+  Post.findAll({
+    attributes: [
+      'id',
+      'post_url',
+      'title',
+      'created_at',
+      [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
+    ],
+    include: [
+      {
+        model: Comment,
+        attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+        include: {
+          model: User,
+          attributes: ['username']
+        }
+      },
+      {
+        model: User,
+        attributes: ['username']
+      }
+    ]
+  })
+    .then(dbPostData => {
+        const posts = dbPostData.map(post => post.get({ plain: true}));
+      // pass a single post object into the homepage template
+      res.render('homepage', { posts });
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
 router.get('/login', (req, res) => {
   if (req.session.loggedIn) {
     res.redirect('/');
